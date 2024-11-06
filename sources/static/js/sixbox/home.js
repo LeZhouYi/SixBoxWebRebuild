ac0fdf58ec7c46d6032e984c191831d059bbe709
LeZhouYi <1909944159@qq.com>
2024-11-07 04:29:47 +0800

    update

--- sources/static/js/sixbox/home.js
+++
@@ -1,5 +0,0 @@
-import { setBackgroundImage } from "./util/render.js";
-
-window.onload = function(){
-//    setBackgroundImage("background_body", "url('/sources?filename=images/background.jpg')");
-};
