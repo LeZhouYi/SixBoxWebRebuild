c5bc059273771c94806b686effac715d91aaf919
mhhu <mhhu@liangzhicn.com>
2024-12-12 20:09:32 +0800

    update

--- sources/static/js/sixbox/file_system/file_text_add.js
+++ sources/static/js/sixbox/file_system/file_text_add.js
@@ -41,14 +41,9 @@ function initMce(mceInputId, initCallBack){
         language:"zh_CN",
         plugins: "preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media code codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help emoticons autosave autoresize",
         toolbar: "code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | \
-        styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
-        table image media charmap emoticons pagebreak insertdatetime preview | fullscreen | bdmap lineheight",
+            styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
+            table image media charmap emoticons pagebreak insertdatetime preview | fullscreen | bdmap lineheight",
         min_height: 400,
-        /*content_css: [ //可设置编辑区内容展示的css，谨慎使用
-            "/static/reset.css",
-            "/static/ax.css",
-            "/static/css.css",
-        ],*/
         fontsize_formats: "12px 14px 16px 18px 24px 36px 48px 56px 72px",
         font_formats: "微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;",
         importcss_append: true,
