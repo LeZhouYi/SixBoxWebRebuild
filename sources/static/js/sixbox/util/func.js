a6fb20c55dbf33fb94254984edc5a7e526166e6a
mhhu <mhhu@liangzhicn.com>
2025-01-04 18:11:27 +0800

    update

--- sources/static/js/sixbox/util/func.js
+++ sources/static/js/sixbox/util/func.js
@@ -58,11 +58,11 @@ function timeStampToText(timeStamp) {
     /*将以秒单位的时间戳转成具体的样式文本*/
     let date = new Date(parseInt(timeStamp) * 1000);
     let year = date.getFullYear();
-    let month = String(date.getMonth() + 1).padStart(2, '0');
-    let day = String(date.getDate()).padStart(2, '0');
-    let hours = String(date.getHours()).padStart(2, '0');
-    let minutes = String(date.getMinutes()).padStart(2, '0');
-    let seconds = String(date.getSeconds()).padStart(2, '0');
+    let month = String(date.getMonth() + 1).padStart(2, "0");
+    let day = String(date.getDate()).padStart(2, "0");
+    let hours = String(date.getHours()).padStart(2, "0");
+    let minutes = String(date.getMinutes()).padStart(2, "0");
+    let seconds = String(date.getSeconds()).padStart(2, "0");
     return `${year}-${month}-${day}` + " " + `${hours}:${minutes}:${seconds}`;
 }
 
@@ -88,28 +88,28 @@ async function clipTextToBoard(clipText) {
         await navigator.clipboard.writeText(clipText);
         return;
     }
-    var isRTL = document.documentElement.getAttribute('dir') === 'rtl';
-    var element = document.createElement('textarea');
+    let isRTL = document.documentElement.getAttribute("dir") === "rtl";
+    let element = document.createElement("textarea");
     // 防止在ios中产生缩放效果
-    element.style.fontSize = '12pt';
+    element.style.fontSize = "12pt";
     // 重置盒模型
-    element.style.border = '0';
-    element.style.padding = '0';
-    element.style.margin = '0';
+    element.style.border = "0";
+    element.style.padding = "0";
+    element.style.margin = "0";
     // 将元素移到屏幕外
-    element.style.position = 'absolute';
-    element.style[isRTL ? 'right' : 'left'] = '-9999px';
+    element.style.position = "absolute";
+    element.style[isRTL ? "right" : "left"] = "-9999px";
     // 移动元素到页面底部
     let yPosition = window.pageYOffset || document.documentElement.scrollTop;
     element.style.top = `${yPosition}px`;
     //设置元素只读
-    element.setAttribute('readonly', '');
+    element.setAttribute("readonly", "");
     element.value = text;
     document.body.appendChild(element);
 
     element.select();
     element.setSelectionRange(0, element.value.length);
-    document.execCommand('copy');
+    document.execCommand("copy");
     element.remove();
 }
 
@@ -131,6 +131,6 @@ function callElementsByClass(elementClass, callback){
 
 function getFullDomain(){
     /*获取当前完整的域名*/
-    var domain = window.location.protocol + '//' + window.location.host;
+    let domain = window.location.protocol + "//" + window.location.host;
     return domain;
 }
