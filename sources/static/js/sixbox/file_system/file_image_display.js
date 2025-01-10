2b0ece8a8519326aa9c2509248decf6f7bd60a48
mhhu <mhhu@liangzhicn.com>
2025-01-10 20:37:32 +0800

    update

--- sources/static/js/sixbox/file_system/file_image_display.js
+++ sources/static/js/sixbox/file_system/file_image_display.js
@@ -42,8 +42,8 @@ callElement("image_download_button", element=>{
     element.addEventListener("click", function(event){
         /*点击下载图片*/
         let accessToken = localStorage.getItem("accessToken");
-        let fileId = localStorage.getItem("nowDisplayId");
-        let downloadUrl = `api/v1/files/${fileId}/download?token=${accessToken}`;
+        let nowControlData = JSON.parse(localStorage.getItem("nowControlData"));
+        let downloadUrl = `api/v1/files/${nowControlData.id}/download?token=${accessToken}`;
         downloadByA(downloadUrl);
     })
 });
@@ -52,13 +52,12 @@ callElement("image_last_button", element=>{
     element.addEventListener("click", async function(event){
         /*切换至上一张图片*/
         let accessToken = localStorage.getItem("accessToken");
-        let fileId = localStorage.getItem("nowDisplayId");
-        let getUrl = `/files/${fileId}/near`;
+        let nowControlData = JSON.parse(localStorage.getItem("nowControlData"));
+        let getUrl = `/files/${nowControlData.id}/near`;
         try{
             let data = await getJsonWithAuth(getUrl);
             if(data.last){
                 callElement("now_display_image", imageElement=>{
-                    localStorage.setItem("nowDisplayId", data.last.id);
                     localStorage.setItem("nowControlData", JSON.stringify(data.last));
                     imageElement.src = `api/v1/files/${data.last.id}/download?token=${accessToken}`;
                 });
@@ -75,13 +74,12 @@ callElement("image_next_button", element=>{
     element.addEventListener("click", async function(event){
         /*切换至上一张图片*/
         let accessToken = localStorage.getItem("accessToken");
-        let fileId = localStorage.getItem("nowDisplayId");
-        let getUrl = `/files/${fileId}/near`;
+        let nowControlData = JSON.parse(localStorage.getItem("nowControlData"));
+        let getUrl = `/files/${nowControlData.id}/near`;
         try{
             let data = await getJsonWithAuth(getUrl);
             if(data.next){
                 callElement("now_display_image", imageElement=>{
-                    localStorage.setItem("nowDisplayId", data.next.id);
                     localStorage.setItem("nowControlData", JSON.stringify(data.next));
                     imageElement.src = `api/v1/files/${data.next.id}/download?token=${accessToken}`;
                 });
@@ -106,7 +104,6 @@ function bindClickImage(element, fileData){
     element.addEventListener("click", function(event){
         displayElementById("image_display_overlay");
         localStorage.setItem("nowControlData", JSON.stringify(fileData))
-        localStorage.setItem("nowDisplayId", fileData.id);
         callElement("now_display_image", element=>{
             let accessToken = localStorage.getItem("accessToken");
             let imageUrl = `api/v1/files/${fileData.id}/download?token=${accessToken}`;
--- sources/static/js/sixbox/music_box/music_box.js
+++ sources/static/js/sixbox/music_box/music_box.js
@@ -12,6 +12,7 @@ window.addEventListener("load",function () {
         initBackground();
     });
     resizeFullScreen();
+    updateMusicList();
 
     window.addEventListener("resize", throttle(function () {
         resizeFullScreen("bodyContainer");
@@ -25,4 +26,73 @@ function checkLocalStorage() {
     if (accessToken === null || refreshToken === null) {
         window.location.href = "/login.html";
     }
+    checkLocalDefault("nowMscSetId", "1");
+}
+
+async function updateMusicList(){
+    /*更新并显示当前音频合集*/
+    let nowPage = localStorage.getItem("nowPage");
+    let pageIndex = parseInt(nowPage) - 1;
+    let nowLimit = localStorage.getItem("nowLimit");
+    let nowMscSetId = localStorage.getItem("nowMscSetId");
+    let data = await getJsonWithAuth(`/musicSets/${nowMscSetId}?_page=${pageIndex}&_limit=${nowLimit}`);
+
+    callElement("music_control_title_text", titleElement=>{
+        titleElement.textContent = data.name;
+    });
+    let contents = data.contents;
+    callElement("music_content_row", rowElement=>{
+        rowElement.innerHTML = null;
+        contents.forEach(content => {
+            rowElement.appendChild(createMusicItem(content));
+        });
+    });
+}
+
+function createMusicItem(content){
+    /*创建单行音频元素*/
+    let musicItem = document.createElement("div");
+    musicItem.classList.add("music_content");
+
+    let musicItemName = document.createElement("div");
+    musicItemName.classList.add("music_item_name_div");
+
+    let musicItemIcon = document.createElement("img");
+    musicItemIcon.classList.add("music_item_name_icon");
+    musicItemIcon.src = "/static/icons/music.png";
+    musicItemName.appendChild(musicItemIcon);
+
+    let musicItmeNameText = document.createElement("div");
+    musicItmeNameText.classList.add("music_item_name_text");
+    musicItmeNameText.textContent = content.name;
+    musicItemName.appendChild(musicItmeNameText);
+
+    musicItem.appendChild(musicItemName);
+
+    let musicItemSinger = document.createElement("div");
+    musicItemSinger.classList.add("music_item_text");
+    musicItemSinger.textContent = content.singer;
+    musicItem.appendChild(musicItemSinger);
+
+    let musicItemAlbum = document.createElement("div");
+    musicItemAlbum.classList.add("music_item_text");
+    musicItemAlbum.textContent = content.album;
+    musicItem.appendChild(musicItemAlbum);
+
+    let musicItemTags = document.createElement("div");
+    musicItemTags.classList.add("music_item_tags_div");
+    musicItemTags.textContent = content.tags;
+    musicItem.appendChild(musicItemTags);
+
+    let musicItemControl = document.createElement("div");
+    musicItemControl.classList.add("music_item_control_div");
+
+    let controlImg = document.createElement("img");
+    controlImg.classList.add("music_item_control_img", "clickable");
+    controlImg.src = "/static/icons/dots.png";
+    musicItemControl.appendChild(controlImg);
+
+    musicItem.appendChild(musicItemControl);
+
+    return musicItem;
 }
