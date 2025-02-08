3508214f7a4980d0a0e5e448b005ff1362e92a90
mhhu <mhhu@liangzhicn.com>
2025-02-08 11:11:01 +0800

    update

--- sources/static/js/sixbox/common/navigation_bar.js
+++ sources/static/js/sixbox/common/navigation_bar.js
@@ -1,6 +1,26 @@
+window.addEventListener("load",function () {
+    clickOverlayHidden("navigation_user_overlay", "navigation_user_content");
+});
+
 function setNavigationBarTitle(title,elementId="navigation_header_title"){
     /*设置导航栏标题*/
     callElement(elementId, element=>{
         element.textContent = title;
     })
 }
+
+callElement("navigate_user_button", element=>{
+    /*点击用户图标*/
+    element.addEventListener("click", function(event){
+        displayElementById("navigation_user_overlay");
+        adjustRelativeLDPopup("navigation_user_content", event.pageX, event.pageY);
+        addObserveResizeHiddenById("navigation_user_overlay");
+    });
+});
+
+callElement("user_logout_button", element=>{
+    /*点击登出*/
+    element.addEventListener("click", function(event){
+        window.location.href = "/login.html";
+    });
+});
